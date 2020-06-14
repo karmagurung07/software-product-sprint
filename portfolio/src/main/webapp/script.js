3c177a8ce09bdd30b6e5366b47c862d3f9ebf514
Karma Gurung <48042602+karmagurung07@users.noreply.github.com>
2020-06-14 16:37:15 -0400

    Add asynGreetingFunction and comment out existing greeting function (step 2 fetch)

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,6 +15,7 @@
 /**
  * Adds a random greeting to the page.
  */
+/*
 function addRandomGreeting() {
   const greetings =
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
@@ -25,4 +26,11 @@ function addRandomGreeting() {
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+*/
+
+async function getRandomGreetingUsingAsyncAwait() {
+  const response = await fetch('/data');
+  const greeting = await response.text();
+  document.getElementById('greeting-container').innerText = greeting;
 }
